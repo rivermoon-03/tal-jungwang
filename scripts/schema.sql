78029a9feac36ca936672588a09d3985bcf6245a
a <riverm00n@naver.com>
2026-04-21 20:47:44 +0900

    .
    .

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -293,9 +293,9 @@ INSERT INTO app_info (id, version, description, feedback_url, updated_at) VALUES
 
 INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (3, '20-1', '시흥20-1번', '아이파크아파트방면', '224000011', '하교');
 INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (2, '시흥33', '시흥33번', '시흥시청방면', '224000062', '하교');
-INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (1, '3400', NULL, '서울행', NULL, '하교');
-INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (6, '6502', NULL, '서울행', NULL, '하교');
-INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (7, '3401', NULL, '서울행', NULL, '하교');
+INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (1, '3400', NULL, '시화터미널 출발 사당 경유 강남행', NULL, '하교');
+INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (6, '6502', NULL, '사당행', NULL, '하교');
+INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (7, '3401', NULL, '시흥시청 경유 석수행', NULL, '하교');
 INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (8, '3400', NULL, '학교행', NULL, '등교');
 INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (9, '6502', NULL, '학교행', NULL, '등교');
 INSERT INTO bus_routes (id, route_number, route_name, direction_name, gbis_route_id, category) VALUES (11, '5602', '5602번', '구로디지털단지역방면', NULL, '하교');
